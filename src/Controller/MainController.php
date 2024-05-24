8d4698cab63da4cd7fbbf7301d794f51146ad245
Jeremie NGOYI MAOLE <ngoyi.jeremie@gmail.com>
2024-05-24 14:39:47 +0200

    Adding all pages components. Matching template with Symfony/Tailwind environnement

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -19,7 +19,39 @@ class MainController extends AbstractController
     #[Route('/about', name: 'app_about')]
     public function about(): Response
     {
-        return $this->render('main/main.html.twig', [
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/pricing', name: 'app_pricing')]
+    public function pricing(): Response
+    {
+        return $this->render('pricing/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/blog', name: 'app_blog')]
+    public function blog(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/blog/detail', name: 'app_blog_detail')]
+    public function blogDetail(): Response
+    {
+        return $this->render('blog/detail.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
